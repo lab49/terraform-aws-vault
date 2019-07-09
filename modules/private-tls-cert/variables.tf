00a0f3682ce669ecaef766621011ec9f771ef088
k504866430 <kruttikagarwal@gmail.com>
2019-07-09 12:51:25 -0400

    Updated variable for lab49 customizations

--- modules/private-tls-cert/variables.tf
+++ modules/private-tls-cert/variables.tf
@@ -5,44 +5,54 @@
 
 variable "ca_public_key_file_path" {
   description = "Write the PEM-encoded CA certificate public key to this path (e.g. /etc/tls/ca.crt.pem)."
+  default = "/Users/kruttik.aggarwal/Documents/workspace-sre/terraform-aws-vault/modules/private-tls-cert/ca.crt.pem"
 }
 
 variable "public_key_file_path" {
   description = "Write the PEM-encoded certificate public key to this path (e.g. /etc/tls/vault.crt.pem)."
+  default = "/Users/kruttik.aggarwal/Documents/workspace-sre/terraform-aws-vault/modules/private-tls-cert/vault.crt.pem"
 }
 
 variable "private_key_file_path" {
   description = "Write the PEM-encoded certificate private key to this path (e.g. /etc/tls/vault.key.pem)."
+  default = "/Users/kruttik.aggarwal/Documents/workspace-sre/terraform-aws-vault/modules/private-tls-cert/vault.key.pem"
 }
 
 variable "owner" {
   description = "The OS user who should be given ownership over the certificate files."
+  default = "kruttik.aggarwal"
 }
 
 variable "organization_name" {
   description = "The name of the organization to associate with the certificates (e.g. Acme Co)."
+  default = "Lab49 LLC"
 }
 
 variable "ca_common_name" {
   description = "The common name to use in the subject of the CA certificate (e.g. acme.co cert)."
+  default = "lab49"
 }
 
 variable "common_name" {
   description = "The common name to use in the subject of the certificate (e.g. acme.co cert)."
+  default = "lab49"
 }
 
 variable "dns_names" {
   description = "List of DNS names for which the certificate will be valid (e.g. vault.service.consul, foo.example.com)."
   type        = "list"
+  default = ["vault.service.consul", "nonpod-vault.lab49aws.com"]
 }
 
 variable "ip_addresses" {
   description = "List of IP addresses for which the certificate will be valid (e.g. 127.0.0.1)."
   type        = "list"
+  default = ["127.0.0.1"]
 }
 
 variable "validity_period_hours" {
   description = "The number of hours after initial issuing that the certificate will become invalid."
+  default = "1"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -90,3 +100,4 @@ variable "private_key_rsa_bits" {
   description = "The size of the generated RSA key in bits. Should only be used if var.private_key_algorithm is RSA."
   default     = "2048"
 }
+
